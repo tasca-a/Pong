4dd1e6fa174ae55139d3929f0813470ffe468e0c
tasca-a <a.tasca@campus.unimib.it>
2022-11-25 18:25:39 +0100

    Added score

--- main.c
+++ main.c
@@ -22,11 +22,13 @@ typedef struct{
 
 typedef struct{
     float x;
-    float y;
+    float y; 
     float x_s;
     float y_s;
     float s;
 } ball;
+
+void draw_number(SDL_Renderer *renderer, int n, int x, int y);
 
 int main(int argc, char **argv)
 {
@@ -39,7 +41,7 @@ int main(int argc, char **argv)
     uint32_t next_game_tick = SDL_GetTicks();
     player p1 = {30, 50, 20, 75, 0, 0};
     player p2 = {350, 50, 20, 75, 0, 0};
-    ball b = {WIDTH/2.0-5, HEIGHT/2.0-5, 0, 0, 5};
+    ball b = {WIDTH/2.0-5, HEIGHT/2.0-5, 0, 0, 3};
     SDL_Rect r_p1 = {0, 0, 20, 75};
     SDL_Rect r_p2 = {0, 0, 20, 75};
     SDL_Rect r_b = {0, 0, 10, 10};
@@ -127,6 +129,9 @@ int main(int argc, char **argv)
         r_b.x = (int) b.x;
         r_b.y = (int) b.y;
         SDL_RenderFillRect(renderer, &r_b);
+        // Render score
+        draw_number(renderer, p1.score, WIDTH/2 - 60, 0);
+        draw_number(renderer, p2.score, WIDTH/2, 0);
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); //background
         SDL_RenderPresent(renderer);
 
@@ -142,4 +147,222 @@ int main(int argc, char **argv)
     SDL_Quit();
 
     return 0;
+}
+
+// Draw a number (0-9) at the provided coordinates (60x60)
+void draw_number(SDL_Renderer *renderer, int n, int x, int y){
+    static SDL_Rect r[5];
+
+    if (n < 0 || n > 9)
+        return;
+
+    switch (n) {
+        case 0: {
+            r[0].x = x+10;
+            r[0].y = y+10;
+            r[0].h = 50;
+            r[0].w = 10;
+            r[1].x = x+20;
+            r[1].y = y+10;
+            r[1].h = 10;
+            r[1].w = 20;
+            r[2].x = x+40;
+            r[2].y = y+10;
+            r[2].h = 50;
+            r[2].w = 10;
+            r[3].x = x+20;
+            r[3].y = y+50;
+            r[3].h = 10;
+            r[3].w = 20;
+            r[4].x = r[4].y = r[4].h = r[4].w = 0;
+        }; break;
+        case 1: {
+            r[0].x = x+20;
+            r[0].y = y+20;
+            r[0].h = r[0].w = 10;
+            r[1].x = x+30;
+            r[1].y = y+10;
+            r[1].h = 40;
+            r[1].w = 10;
+            r[2].x = x+20;
+            r[2].y = y+50;
+            r[2].h = 10;
+            r[2].w = 30;
+            r[3].x = r[3].y = r[3].h = r[3].w = 0;
+            r[4].x = r[4].y = r[4].h = r[4].w = 0;
+        }; break;
+        case 2: {
+            r[0].x = x+10;
+            r[0].y = y+10;
+            r[0].h = 10;
+            r[0].w = 40;
+            r[1].x = x+40;
+            r[1].y = y+20;
+            r[1].h = 20;
+            r[1].w = 10;
+            r[2].x = x+10;
+            r[2].y = y+30;
+            r[2].h = 10;
+            r[2].w = 30;
+            r[3].x = x+10;
+            r[3].y = y+40;
+            r[3].h = 20;
+            r[3].w = 10;
+            r[4].x = x+20;
+            r[4].y = y+50;
+            r[4].h = 10;
+            r[4].w = 30;
+        }; break;
+        case 3: {
+            r[0].x = x+10;
+            r[0].y = y+10;
+            r[0].h = 10;
+            r[0].w = 40;
+            r[1].x = x+40;
+            r[1].y = y+20;
+            r[1].h = 30;
+            r[1].w = 10;
+            r[2].x = x+20;
+            r[2].y = y+30;
+            r[2].h = 10;
+            r[2].w = 20;
+            r[3].x = x+10;
+            r[3].y = y+50;
+            r[3].h = 10;
+            r[3].w = 40;
+            r[4].x = r[4].y = r[4].h = r[4].w = 0;
+        }; break;
+        case 4: {
+            r[0].x = x+30;
+            r[0].y = y+10;
+            r[0].h = 50;
+            r[0].w = 10;
+            r[1].x = x+20;
+            r[1].y = y+20;
+            r[1].h = 10;
+            r[1].w = 10;
+            r[2].x = x+10;
+            r[2].y = y+30;
+            r[2].h = 10;
+            r[2].w = 10;
+            r[3].x = x+10;
+            r[3].y = y+40;
+            r[3].h = 10;
+            r[3].w = 40;
+            r[4].x = r[4].y = r[4].h = r[4].w = 0;
+        }; break;
+        case 5: {
+            r[0].x = x+10;
+            r[0].y = y+10;
+            r[0].h = 10;
+            r[0].w = 40;
+            r[1].x = x+10;
+            r[1].y = y+20;
+            r[1].h = 20;
+            r[1].w = 10;
+            r[2].x = x+20;
+            r[2].y = y+30;
+            r[2].h = 10;
+            r[2].w = 30;
+            r[3].x = x+40;
+            r[3].y = y+40;
+            r[3].h = 20;
+            r[3].w = 10;
+            r[4].x = x+10;
+            r[4].y = y+50;
+            r[4].h = 10;
+            r[4].w = 30;
+        }; break;
+        case 6: {
+            r[0].x = x+20;
+            r[0].y = y+10;
+            r[0].h = 10;
+            r[0].w = 30;
+            r[1].x = x+10;
+            r[1].y = y+10;
+            r[1].h = 50;
+            r[1].w = 10;
+            r[2].x = x+20;
+            r[2].y = y+30;
+            r[2].h = 10;
+            r[2].w = 30;
+            r[3].x = x+20;
+            r[3].y = y+50;
+            r[3].h = 10;
+            r[3].w = 30;
+            r[4].x = x+40;
+            r[4].y = y+40;
+            r[4].h = 10;
+            r[4].w = 10;
+        }; break;
+        case 7: {
+            r[0].x = x+10;
+            r[0].y = y+10;
+            r[0].h = 10;
+            r[0].w = 40;
+            r[1].x = x+40;
+            r[1].y = y+20;
+            r[1].h = 10;
+            r[1].w = 10;
+            r[2].x = x+30;
+            r[2].y = y+30;
+            r[2].h = 10;
+            r[2].w = 10;
+            r[3].x = x+20;
+            r[3].y = y+40;
+            r[3].h = 10;
+            r[3].w = 10;
+            r[4].x = x+10;
+            r[4].y = y+50;
+            r[4].h = 10;
+            r[4].w = 10;
+        }; break;
+        case 8: {
+            r[0].x = x+10;
+            r[0].y = y+10;
+            r[0].h = 50;
+            r[0].w = 10;
+            r[1].x = x+20;
+            r[1].y = y+10;
+            r[1].h = 10;
+            r[1].w = 20;
+            r[2].x = x+40;
+            r[2].y = y+10;
+            r[2].h = 50;
+            r[2].w = 10;
+            r[3].x = x+20;
+            r[3].y = y+30;
+            r[3].h = 10;
+            r[3].w = 20;
+            r[4].x = x+20;
+            r[4].y = y+50;
+            r[4].h = 10;
+            r[4].w = 20;
+        }; break;
+        case 9: {
+            r[0].x = x+10;
+            r[0].y = y+10;
+            r[0].h = 30;
+            r[0].w = 10;
+            r[1].x = x+20;
+            r[1].y = y+10;
+            r[1].h = 10;
+            r[1].w = 20;
+            r[2].x = x+40;
+            r[2].y = y+10;
+            r[2].h = 50;
+            r[2].w = 10;
+            r[3].x = x+20;
+            r[3].y = y+30;
+            r[3].h = 10;
+            r[3].w = 20;
+            r[4].x = x+10;
+            r[4].y = y+50;
+            r[4].h = 10;
+            r[4].w = 30;
+        }; break;
+    }
+
+    for (int i = 0; i < 5; i++)
+        SDL_RenderFillRect(renderer, &r[i]);
 }
