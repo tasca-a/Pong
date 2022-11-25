8f4111faf84575bf38048f223557aa67d330049c
tasca-a <a.tasca@campus.unimib.it>
2022-11-25 15:28:15 +0100

    Code refactor and added bouncy ball

--- main.c
+++ main.c
@@ -1,250 +1,145 @@
 #include <SDL2/SDL_events.h>
-#include <SDL2/SDL_hints.h>
-#include <SDL2/SDL_keyboard.h>
-#include <SDL2/SDL_keycode.h>
+#include <SDL2/SDL_log.h>
 #include <SDL2/SDL_rect.h>
 #include <SDL2/SDL_render.h>
-#include <SDL2/SDL_stdinc.h>
-#include <SDL2/SDL_timer.h>
-#include <SDL2/SDL_video.h>
-#include <SDL2/SDL.h>
-
+#include <stdint.h>
 #include <stdio.h>
+#include <SDL2/SDL.h>
 #include <stdlib.h>
+#include <time.h>
+
+#define WIDTH 400
+#define HEIGHT 300
 
 typedef struct{
-    SDL_Rect *body;
-    int velocity;
-    int speed;
-} Player;
+    float x;
+    float y;
+    int w;
+    int h;
+    float speed;
+    int score;
+} player;
 
 typedef struct{
-    SDL_Rect *body;
-    float angle;
-    int speed;
-    int direction;
-} Ball;
-
-#define TRUE 1
-#define FALSE 0
-
-static const int WIDTH = 400;
-static const int HEIGHT = 300;
-//static const int SPEED = 3;
-
-static SDL_Window *window = NULL;
-static SDL_Renderer *renderer = NULL;
-static Player *player1 = NULL;
-static Player *player2 = NULL;
-static Ball *ball = NULL;
-
-void initialize();
-void cleanup();
-void handle_key_press(SDL_Keysym *keysym, SDL_EventType type);
-void handle_player_movements(Player *player);
-void handle_ball_movement(Ball *ball);
-void bounce(Ball *b);
-void render_player(Player *player);
-void render_ball(Ball *ball);
-
-Player *CreatePlayer(int x, int y, int width, int height, int speed);
-Ball *CreateBall(int size);
+    float x;
+    float y;
+    float x_s;
+    float y_s;
+    float s;
+} ball;
 
 int main(int argc, char **argv)
 {
-    int isRunning = TRUE;
-    initialize();
+    int game_running = 1;
 
-    int sleep = 0;
-    Uint32 next_game_tick = SDL_GetTicks();
+    // Initialization
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *window = SDL_CreateWindow("Pong", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_OPENGL);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    uint32_t next_game_tick = SDL_GetTicks();
+    player p1 = {30, 50, 20, 75, 0, 0};
+    player p2 = {350, 50, 20, 75, 0, 0};
+    ball b = {WIDTH/2.0-5, HEIGHT/2.0-5, 0, 0, 5};
+    SDL_Rect r_p1 = {0, 0, 20, 75};
+    SDL_Rect r_p2 = {0, 0, 20, 75};
+    SDL_Rect r_b = {0, 0, 10, 10};
+    srand(time(NULL));
 
-    // Game loop
-    while (isRunning){
-        // Clear the previous rendered frame
+    b.x_s = b.s;    // Initial kick
+
+    while (game_running){
+        // Clear frame
         SDL_RenderClear(renderer);
 
-        // Catch input
+        // Check conditions and set flags (victory, score, etc.)
+
+        // Get input
         SDL_Event event;
         while (SDL_PollEvent(&event)){
             switch (event.type) {
-                case SDL_QUIT: isRunning = FALSE; break;
-                case SDL_KEYDOWN: handle_key_press(&event.key.keysym, SDL_KEYDOWN); break;
-                case SDL_KEYUP: handle_key_press(&event.key.keysym, SDL_KEYUP); break;
+                case SDL_QUIT: game_running = 0; break;
+                case SDL_KEYDOWN:
+                    switch(event.key.keysym.sym){
+                        // Player 1
+                        case SDLK_w: p1.speed = -4; break;
+                        case SDLK_s: p1.speed = +4; break;
+                        // Player 2
+                        case SDLK_o: p2.speed = -4; break;
+                        case SDLK_l: p2.speed = +4; break;
+                    }; break;
+                case SDL_KEYUP:
+                    switch(event.key.keysym.sym){
+                        case SDLK_w: p1.speed = 0; break;
+                        case SDLK_s: p1.speed = 0; break;
+                        case SDLK_o: p2.speed = 0; break;
+                        case SDLK_l: p2.speed = 0; break;
+                    }; break;
             }
         }
 
-        // Handle the movements
-        handle_player_movements(player1);
-        handle_player_movements(player2);
-        handle_ball_movement(ball);
+        // Movements
+        if (p1.y + p1.speed > 0 && p1.y + p1.h + p1.speed < HEIGHT)
+            p1.y += p1.speed;
+        if (p2.y + p2.speed > 0 && p2.y + p2.h + p2.speed < HEIGHT)
+            p2.y += p2.speed;
+        b.x += b.x_s;
+        b.y += b.y_s;
+        
 
-        // Render players
-        render_player(player1);
-        render_player(player2);
+        // Game logic
+        if (b.x <= p1.x + p1.w && b.y < p1.y + p1.h && b.y + 10 > p1.y){ // bounce on p1
+            b.x_s *= -1;
+            b.y_s = ((float) rand() / (float) RAND_MAX) * 6 - 3;
+        }
+        if (b.x + 10 > p2.x && b.y < p2.y + p2.h && b.y + 10 > p2.y){    // bounce on p2
+            b.x_s *= -1;
+            b.y_s = ((float) rand() / (float) RAND_MAX) * 6 - 3;
+        }
+        if (b.y < 0 || b.y + 10 > HEIGHT) // up down ball bounce
+            b.y_s *= -1;
+        if (b.x < 0){
+            // p2 score
+            p2.score++;
+            b.x = WIDTH/2.0-5;
+            b.y = HEIGHT/2.0-5;
+            b.y_s = 0;
 
-        // Render the ball
-        render_ball(ball);
+            SDL_Log("score =\tp1 = %d\tp2 = %d\n", p1.score, p2.score);
+        }
+        if (b.x + 10 > WIDTH){
+            // p1 score
+            p1.score++;
+            b.x = WIDTH/2.0-5;
+            b.y = HEIGHT/2.0-5;
+            b.y_s = 0;
 
-        // Present the new frame
+            SDL_Log("score =\tp1 = %d\tp2 = %d\n", p1.score, p2.score);
+        }
+
+        // Render frame
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        r_p1.x = (int) p1.x;
+        r_p1.y = (int) p1.y;
+        SDL_RenderFillRect(renderer, &r_p1);
+        r_p2.x = (int) p2.x;
+        r_p2.y = (int) p2.y;
+        SDL_RenderFillRect(renderer, &r_p2);
+        r_b.x = (int) b.x;
+        r_b.y = (int) b.y;
+        SDL_RenderFillRect(renderer, &r_b);
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); //background
         SDL_RenderPresent(renderer);
 
-        //Wait for the next tick
+        // Wait for the next frame
         next_game_tick += 1000/60;
-        sleep = next_game_tick - SDL_GetTicks();
+        int32_t sleep = next_game_tick - SDL_GetTicks();
+
         if (sleep > 0)
             SDL_Delay(sleep);
     }
 
-    cleanup();
-    return 0;
-}
-
-// Perform all the necessary initializations of SDL
-void initialize(){
-    // Initialize SDL
-    SDL_Init(SDL_INIT_VIDEO);
-
-    // Create the window
-    window = SDL_CreateWindow(
-        "Pong", 
-        SDL_WINDOWPOS_UNDEFINED, 
-        SDL_WINDOWPOS_UNDEFINED, 
-        WIDTH, 
-        HEIGHT, 
-        SDL_WINDOW_OPENGL
-    );
-
-    // Create renderer
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
-    // Create players
-    player1 = CreatePlayer(30, 50, 20, 75, 3);
-    player2 = CreatePlayer(350, 50, 20, 75, 3);
-
-    // Create ball
-    ball = CreateBall(10);
-}
-
-// Perform all the necessary cleanups before quitting
-void cleanup(){
-    free(player1);
-    free(player2);
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
+    // Cleanup
     SDL_Quit();
-}
 
-// Handle all the keyboard input
-void handle_key_press(SDL_Keysym *keysym, SDL_EventType type){
-    if (type == SDL_KEYDOWN)
-        switch (keysym->sym) {
-            // Player1
-            case SDLK_w: player1->velocity = -1; break;
-            case SDLK_s: player1->velocity = +1; break;
-
-            // Player2
-            case SDLK_o: player2->velocity = -1; break;
-            case SDLK_l: player2->velocity = +1; break;
-        }
-
-    if (type == SDL_KEYUP)
-        switch (keysym->sym) {
-            // Player1
-            case SDLK_w: player1->velocity = 0; break;
-            case SDLK_s: player1->velocity = 0; break;
-
-            // Player2
-            case SDLK_o: player2->velocity = 0; break;
-            case SDLK_l: player2->velocity = 0; break;
-        }
-}
-
-// Handle the player movements
-void handle_player_movements(Player *player){
-    if (player->body->y + player->velocity * player->speed > 0 &&
-            player->body->y + player->body->h + player->velocity * player->speed < HEIGHT)
-            player->body->y += player->velocity * player->speed;
-}
-
-// Handle all the ball movements
-void handle_ball_movement(Ball *b){
-    int *x = &b->body->x;
-    int *y = &b->body->y;
-    int *speed = &b->speed;
-    float *angle = &b->angle;
-    int *direction = &b->direction;
-    int size = b->body->w;
-
-    // Check collision with walls
-    if ((*x) >  WIDTH - size)
-        bounce(b);
-    
-    if ((*x) < 0)
-        bounce(b);
-
-    // Check collision with players
-    if ((*x) < player1->body->x + player1->body->w &&
-        (*y) + size > player1->body->y &&
-        (*y) < player1->body->y + player1->body->h)
-        bounce(b);
-
-    if ((*x) + size > player2->body->x&&
-        (*y) + size > player2->body->y &&
-        (*y) < player2->body->y + player2->body->h)
-        bounce(b);
-
-
-    // Move the ball in a line
-    (*x) += (*direction) * (*speed);
-    //(*y) += (int) (*angle) * (*x);
-}
-
-// Make the ball bounce, inverting the direction of the movement
-void bounce(Ball *b){
-    b->direction *= -1;
-}
-
-// Render the player to the screen
-void render_player(Player *player){
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderFillRect(renderer, player->body);
-}
-
-// Render the ball to the screen
-void render_ball(Ball *ball){
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderFillRect(renderer, ball->body);
-}
-
-// Return a pointer to a newly created player
-Player *CreatePlayer(int x, int y, int width, int height, int speed){
-    Player *p = (Player*) malloc(sizeof(Player));;
-    p->body = (SDL_Rect*) malloc(sizeof(SDL_Rect));
-
-    p->body->x = x;
-    p->body->y = y;
-    p->body->w = width;
-    p->body->h = height;
-    p->speed = speed;
-
-    return p;
-}
-
-// Return a pointer to a newly created ball
-Ball *CreateBall(int size){
-    Ball *b = (Ball*) malloc(sizeof(Ball));
-    b->body = (SDL_Rect*) malloc(sizeof(SDL_Rect));
-
-    b->body->x = (WIDTH/2) - (size/2);
-    b->body->y = (HEIGHT/2) - (size/2);
-    b->body->h = size;
-    b->body->w = size;
-
-    b->angle = 1.0f;
-    b->speed = 2;
-    b->direction = 1;
-
-    return b;
+    return 0;
 }
